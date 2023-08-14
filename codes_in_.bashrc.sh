3d5291e744ecb7bcefbb915cd72f5ecb439aa230
abdulbadii <abdulbadiik@gmail.com>
2023-08-15 03:48:14 +0700

    Update codes_in_.bashrc.sh

--- codes_in_.bashrc.sh
+++ codes_in_.bashrc.sh
@@ -1,28 +1,26 @@
 PS1='`echo -e "$DIRS"`\[\e[41;1;37m\]\w\[\e[40;1;33m\]\n\$\[\e[m\] '
 g(){
 eval "n=\$$#"
-[[ $n = *, ]] &&{ ((HIDIRF=1-HIDIRF))
- (($#>1)) &&set -- ${@:1:(($#-1))}
+[[ $n = , ]] &&{ ((HIDIRF=1-HIDIRF));(($#>1)) &&set -- ${@:1:(($#-1))}
 }
 case $1 in
 .) pushd -0;;
 -) pushd ~-;;
 1)	popd 2>/dev/null;;
-0) set -- ${DIRSTACK[@]}; for i;{ pushd "$i" ;};;
+0) for i in ${DIRSTACK[@]};{ pushd "$i" ;};;
 0[1-9]*-) n=${1%-}; while popd +$n 2>/dev/null ;do :;done;;
 0[1-9]*-[1-9]*) m=${1%-*};n=${1#*-}; for((i=n-m;i>=0;--i)) ;{ popd +$m 2>/dev/null ;};;
-0[1-9]*) i=;for n;{ [[ $n = 0[1-9] ]] ||break; popd +$((n-i++)) 2>/dev/null ||break;};;
+0[1-9]*) i=;for n;{ [[ $n = 0[1-9]* ]] ||break; popd +$((n-i++)) 2>/dev/null ||break;};;
 00) dirs -c;DIRS=;return;;
 [1-9]|[1-9][0-9])
  m=;[ -d "$1" ]&&{
   m="Directory $1/ exists, if it's meant instead, append character '/' on CLI: m $1/\n"
   n="Into directory $1/, since no index $1 in directory list"
  }
- if 2>/dev/null pushd ${DIRSTACK[$1]};then echo -ne $m>&2
+ if 2>/dev/null pushd "${DIRSTACK[$1]}";then echo -ne $m>&2
  else [[ $m ]] &&{ pushd "$1";echo $n>&2;}
  fi;;
-$PWD);;
-,);;
+,|$PWD);;
 ?*)
  if type -a "$1" 2>/dev/null;then
    F=1
@@ -32,7 +30,7 @@ $PWD);;
     [[ $o = [dD] ]] && F=
    }
    ((F)) &&{
-    exe=$1;args=;DNO=;shift
+    x=$1;args=;DNO=;shift
     for m;{
      [[ $m = -- ]] && DNO=1
      if [[ $m != -* ]] || ((DNO)) && [[ $m =~ ^([^1-9]*)([1-9][0-9]?)(.*) ]] ;then
@@ -40,19 +38,18 @@ $PWD);;
       n=${BASH_REMATCH[2]}
       b=${BASH_REMATCH[3]}
       if [[ $b =~ ^//.|^/$ ]] ;then
-       b=${b#/};dirn=$f$n${b%/}
-       [[ -d $dirn ]] ||{ echo "Cannot stat '$dirn'">&2;return 1;}
+       b=${b/\/\//\/};dirn=$f$n${b%/}
        args=$args\ $dirn
       else
         if ((n<=${#DIRSTACK[@]})) ;then
-         dirn=$f${DIRSTACK[$n]}
-         if [[ $m = */ ]] ;then echo -n $exe>&2;read -ei "$args $dirn" args
+         dirn=$f${DIRSTACK[n]}
+         if [[ $m = */ ]] ;then echo -n $x>&2;read -ei "$args $dirn" args
          else args=$args\ $dirn$b;fi
        else
          echo -n "In '$m', $n is out of dir. stack range"
          if [[ -d $m ]] ;then
-          echo -e " while it's an existing directory\nIf it's really meant append '/' on CLI: '$f$n/$b'"
-          read -N1 -p "So proceed with '$n' treated as the directory? (y / ELSE KEY)" o
+          echo -e " while it's an existing directory\nIf it's really meant directory, append '/' on CLI: '$f$n/$b'"
+          read -N1 -p "So proceed with '$n' as directory oy abort? (y / ELSE KEY)" o
           [[ $o = [yY] ]] ||return 1
           args=$args\ $m
          else return 1;fi
@@ -60,26 +57,24 @@ $PWD);;
        fi
      else args=$args\ $m;fi
      }
-     eval "$exe $args"
+     eval "$x $args"
     }
 else
-  C=$PWD
-  i=$#
-  if ((i>1)) ;then
+  if (((i=$#)>1)) ;then C=$PWD
    while ((i)) ;do
     eval "n=\${$((i--))}"
     if [[ $n = /* ]] ;then [[ -d $n ]] &&pushd "$n" 
     else [[ -d $C/$n ]] &&pushd "$C/$n";fi
    done
    pushd -0;pushd ~-
-  else pushd $1;fi
+  else pushd "$1";fi
 fi;;
 *) [[ $HOME = $PWD ]] ||pushd ~
 esac
- IFS=$'\n'
+IFS=$'\n'
 l=`dirs -l -p`
 l=$'\n'${l//$'\n'/$'\n\n'}$'\n'
-unset o d DIRS
+o=
 while :
 do set -- $l
 	l=${l//$'\n'$1$'\n'}
@@ -88,9 +83,10 @@ do set -- $l
 done
 cd $1
 dirs -c
-unset IFS C;eval set -- $o
+unset IFS C d DIRS
+eval set -- $o
 for i ;{ pushd "$i" 2>/dev/null ||C=$C\ $1;}
- [[ $C ]]&&echo "Directory stack is wthout just removed'${C// /,}'">&2
+ [[ $C ]]&&echo "Directory stack was cleaned up of just removed'${C// /,}'">&2
 {
  read l
  while read l
@@ -98,6 +94,5 @@ for i ;{ pushd "$i" 2>/dev/null ||C=$C\ $1;}
   d="$d\e[41;1;37m${BASH_REMATCH[1]}\e[40;1;32m${BASH_REMATCH[2]}\e[m "
  done
 }< <(dirs -v);
-DIRS=
 ((HIDIRF)) ||export DIRS=$(echo -e "${d:+$d\n\r}")
 }>/dev/null
