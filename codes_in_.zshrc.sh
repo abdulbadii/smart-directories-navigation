fa06749db4c204daa53537d869775cf755fc3634
abdulbadii <abdulbadiik@gmail.com>
2023-08-17 03:03:34 +0700

    fix bug

--- codes_in_.zshrc.sh
+++ codes_in_.zshrc.sh
@@ -12,7 +12,7 @@ case $1 in
 0[1-9]*-) n=${1%-}; while popd +$n 2>/dev/null ;do :;done;;
 0[1-9]*-[1-9]*) m=${1%-*};n=${1#*-}; for ((i=n-m;i>=0;--i)) ;{ popd +$m 2>/dev/null ;};;
 0[1-9]*) i=;for n;{ [[ $n = 0[1-9]* ]] ||break; popd +$((n-i++)) 2>/dev/null ||break};;
-00) dirs -c;export PS1=$(echo -e "\e[41;1;37m%~\e[40;1;33m\n%%\e[m ");return;;
+00) dirs -c;PS1=$(echo -e "\e[41;1;37m%~\e[40;1;33m\n%%\e[m ");return;;
 [1-9]|[1-9][0-9])
  m=;[ -d "$1" ]&&{
   m="Directory $1/ exists, if it's meant instead, append character '/' on CLI: m $1/\n"
@@ -21,46 +21,45 @@ case $1 in
  if 2>/dev/null pushd "${DIRST[$1]}";then echo -ne $m>&2
  else [[ $m ]] &&{ pushd "$1";echo $n>&2}
  fi;;
-,) DIRS=;((HIDIRF)) || DIRS=$DIRSB
- PS1=$(echo -e "$DIRS\e[41;1;37m%~\e[40;1;33m\n%%\e[m ")
- return;;
-$PWD) DIRS=;return;;
+,) if ((HIDIRF)) ;then echo NOW DIRECTORY STACK LIST IS HIDDEN>&2;DIRS=
+  else DIRS=$DIRSB ;fi
+  PS1=$(echo -e "$DIRS\e[41;1;37m%~\e[40;1;33m\n%%\e[m ");return;;
+$PWD)return;;
 ?*)
- if type -a "$1" 2>/dev/null;then
-   F=1
+ if type -a "$1" 2>/dev/null;then F=1
    [[ -d $1 ]] &&{
     echo "'$1' is a directory in the working dir. but it's a name of an executable too"
-    read -N1 -p 'Is it meant as executable or a directory name (that must have '/' suffix on CLI) ? (d / ELSE KEY) ' o
-    [[ $o = [dD] ]] && F=
+    read -N1 -p 'Is it an executable or directory name (which must be appended with / on CLI) ? (x / ELSE KEY) ' o
+    [[ $o = [xX] ]] ||{ pushd  $1;F= }
    }
    ((F)) &&{
-    x=$1;args=;DNO=;shift
-    if [[ $1 = - ]];then args=$args\ $PWD;shift
-    elif [[ $1 = . ]];then args=$args\ ${DIRST: -1};shift
-    fi
+    x=$1;shift;args=;DNO=
+    [[ $1 = . ]] &&{ args= ${DIRS: -1};shift}
     for m;{
      [[ $m = -- ]] && DNO=1
-     if [[ $m != -* ]] || ((DNO)) && [[ $m =~ "^([^1-9]*)([1-9][0-9]?)(.*)" ]] ;then
+     if [[ $m =~ "^-(/.*)?" ]];then b=${match[1]}
+      if [[ $m = */ ]] ;then echo -n $x>&2
+      args="$args ~-$b"
+      vared args
+      else args=$args\ ~-$b;fi
+     elif [[ $m != -* ]] || ((DNO)) && [[ $m =~ "^([^1-9]*)([1-9][0-9]?)(.*)" ]] ;then
       f=${match[1]}
       n=${match[2]}
       b=${match[3]}
       if [[ $b =~ "^//.|^/$" ]] ;then
-       b=${b/\/\//\/};dirn=$f$n${b%/}
-       args=$args\ $dirn
+       b=${b/\/\//\/}
+       args=$args\ $f$n${b%/}
       else
         if ((n<=$#DIRST)) ;then
-         dirn=$f$DIRST[n]
-         if [[ $m = */ ]] ;then
-          echo -n $x>&2;read -ei "$args $dirn" args
-         else args=$args\ $dirn$b;fi
+         dirn=$f$DIRSTACK[n]$b
+         if [[ $m = */ ]] ;then echo -n $x>&2
+         args="$args $dirn"
+         vared args
+         else args=$args\ $dirn;fi
        else
          echo -n "In '$m', $n is out of dir. stack range"
-         if [[ -d $m ]] ;then
-          echo -e " while it's an existing directory\nIf it's really meant directory, append '/' on CLI: '$f$n/$b'"
-          read -N1 -p "So proceed with '$n' as directory oy abort? (y / ELSE KEY)" o
-          [[ $o = [yY] ]] ||return 1
-          args=$args\ $m
-         else return 1;fi
+         [[ -d $m ]] && echo ", while it's a directory. Will not proceed"
+         echo -e "\nTo predetermine a number in argument isn't any of dir. stack, append '/' on CLI:\n'$f$n/$b'"
         fi
        fi
      else args=$args\ $m;fi
@@ -101,6 +100,7 @@ W=;for d;{ pushd "$d" 2>/dev/null ||C=$C\ $1;}
  done
 }< <(dirs -v)
 DIRSB=$(echo -e "${d:+$d\n\r}")
-DIRS=;((HIDIRF)) || DIRS=$DIRSB
-PS1=$(echo -e "$DIRS\e[41;1;37m%~\e[40;1;33m\n%%\e[m ")
+ if ((HIDIRF)) ;then echo ..HIDING DIRECTORY STACK LIST>&2;DIRS=
+ else DIRS=$DIRSB ;fi
+ PS1=$(echo -e "$DIRS\e[41;1;37m%~\e[40;1;33m\n%%\e[m ")
 }>/dev/null
