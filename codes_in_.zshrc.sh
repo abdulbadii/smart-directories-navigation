c8aafff7bfcf8ece758c950988bee7b5ff9ee653
abdulbadii <abdulbadiik@gmail.com>
2023-08-22 22:13:22 +0700

    fix bug

--- codes_in_.zshrc.sh
+++ codes_in_.zshrc.sh
@@ -5,34 +5,32 @@ IFS=$'\n'
 d=(`dirs -pl`)
 DIRST=(${d:1})
 case $1 in
-.) pushd -0;;
--) pushd ~-;;
-1)	popd 2>/dev/null;;
-0) for m in $DIRST ;{ pushd "$m"};;
+1) popd -q 2>/dev/null;;
+0) for m in $DIRST ;{ pushd -q "$m"};;
 0[1-9]*-) n=${1%-}; while popd +$n 2>/dev/null ;do :;done;;
-0[1-9]*-[1-9]*) m=${1%-*};n=${1#*-}; for ((i=n-m;i>=0;--i)) ;{ popd +$m 2>/dev/null ;};;
-0[1-9]*) i=;for n;{ [[ $n = 0[1-9]* ]] ||break; popd +$((n-i++)) 2>/dev/null ||break};;
+0[1-9]*-[1-9]*) m=${1%-*};n=${1#*-}; for ((i=n-m;i>=0;--i)) ;{ popd -q +$m 2>/dev/null ;};;
+0[1-9]*) i=;for n;{ [[ $n = 0[1-9]* ]] ||break; popd -q +$((n-i++)) 2>/dev/null ||break};;
 00) dirs -c;PS1="%F{015}%K{001}%~$N%F{011}%K{004}%%%F{015}%K{000} ";return;;
 [1-9]|[1-9][0-9])
  m=;[ -d "$1" ]&&{
   m="Directory $1/ exists, if it's meant instead, append character '/' on CLI: m $1/\n"
   n="Into directory $1/, since no index $1 in directory list"
  }
- if 2>/dev/null pushd "${DIRST[$1]}";then echo -ne $m>&2
- else [[ $m ]] &&{ pushd "$1";echo $n>&2}
+ if 2>/dev/null pushd -q "${DIRST[$1]}";then echo -ne $m
+ else [[ $m ]] &&{ pushd -q "$1";echo $n}
  fi;;
 ,) if ((HIDIRF)) ;then
-  echo NOW DIRECTORY STACK LIST IS HIDDEN>&2
+  echo NOW DIRECTORY STACK LIST IS HIDDEN
   PS1="%F{015}%K{001}%~$N%F{011}%K{004}%%%F{015}%K{000} "
  else PS1="$DIRS%F{015}%K{001}%~$N%F{011}%K{004}%%%F{015}%K{000} ";fi
  return;;
 $PWD)return;;
 ?*)
- if type -a "$1" 2>/dev/null;then F=1
+ if type -a "$1">/dev/null 2>&1 &&[[ $1 != . ]];then F=1
   [[ -d $1 ]] &&{
    echo "'$1' is a directory in the working dir. but it's a name of an executable too"
-   read -N1 -p 'Is it an executable or directory name (which must be appended with / on CLI) ? (x / ELSE KEY) ' o
-   [[ $o = [xX] ]] ||{ pushd  $1;F= }}
+   read -k1 '?Is it an executable or directory name (which must be appended with / on CLI) ? (x / ELSE KEY) ' o
+   [[ $o = [xX] ]] ||{ pushd -q  $1;F= }}
   ((F)) &&{
    x=$1;shift;args=;DNO=
    [[ $1 = . ]] &&{ args=\ ${DIRST: -1};shift}
@@ -60,20 +58,32 @@ $PWD)return;;
      fi
     else args=$args\ $m;fi
    }
-   echo -e "\n$x$args">&2
-   eval "$x$args">&2
+   echo -e "\n$x$args"
+   eval "$x$args"
   }
-else
- if (((i=$#)>1)) ;then C=$PWD
-  while ((i)) ;do
-   eval "n=\${$((i--))}"
-   if [[ $n = /* ]] ;then [[ -d $n ]] &&pushd "$n" 
-   else [[ -d $C/$n ]] &&pushd "$C/$n";fi
-  done
-  pushd -0;pushd ~-
- else pushd "$1";fi
+else D=1
+ [[ $1 = [-.] ]] &&{ D=
+  if [[ $1 = . ]];then pushd -q -0;else pushd -q ~-;fi;shift
+ }
+ i=$#;C=$PWD
+ pushd -q +1
+ while ((i)) ;do
+  eval "n=\${$((i--))}"
+  [[ $n != /* ]] && n="$C/$n" 
+  [[ -d $n ]] ||{ G=
+   echo "'$n' is not a directory"
+   n=${n%/*};n=$n/
+   [[ -d $n ]] ||continue
+   if ((i+D));then G="Put directory '$n' onto stack"
+   else G='Go to the directory '$n'';fi
+   read -k1 "?$G ? (n: No. ELSE KEY: Yes) " o;echo
+   [[ $o = n ]] &&continue}
+  pushd -q "$n" 
+ done
+ pushd -q -0
+ ((D)) &&pushd -q
 fi;;
-*) [[ $HOME = $PWD ]] ||pushd ~
+*) [[ $HOME = $PWD ]] ||pushd -q ~
 esac
 l=`dirs -pl`
 l=$'\n'${l//$'\n'/$'\n\n'}$'\n'
@@ -87,18 +97,18 @@ done
 cd $1
 dirs -c
 eval set -- $=o
-W=;for d;{ pushd "$d" 2>/dev/null ||C=$C\ $1;}
- [[ $C ]]&&echo "Directory stack was cleaned up of just removed'${C// /,}'">&2
+C=;for d;{ pushd -q "$d" 2>/dev/null ||C=$C\ $1;}
+ [[ $C ]]&&echo "Directory stack was cleaned up of just removed'${C// /,}'"
 {
  read l
  d=;while read l
  do [[ $l =~ "^([1-9]+)\h+(.+)" ]]
-  d="$d%F{015}%K{001}${match[1]}%F{010}%K{000} ${match[2]}%F{0015}%K{000} "
+  d="$d%F{015}%K{001}${match[1]}%F{010}%K{000}${match[2]}%F{0015}%K{000} "
  done
 }< <(dirs -v)
  N=$'\n'
  DIRS=${d:+$d$N}
- if ((HIDIRF)) ;then echo ..HIDING DIRECTORY STACK LIST>&2;D=
+ if ((HIDIRF)) ;then echo ..HIDING DIRECTORY STACK LIST;D=
  else D=$DIRS ;fi
  PS1="$D%F{015}%K{001}%~$N%F{011}%K{004}%%%F{015}%K{000} "
-}>/dev/null
+}
