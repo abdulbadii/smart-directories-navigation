7a2ace80456a4df8e12c578e999ae6b22626f5db
abdulbadii <abdulbadiik@gmail.com>
2023-09-10 17:48:45 +0700

    Update bash-completion.bash

--- bash-completion.bash
+++ bash-completion.bash
@@ -1,17 +1,23 @@
-local cword=$COMP_CWORD words=(${COMP_WORDS[@]}) now pre
-now=${words[cword]}
-pre=${words[cword-1]}
-if type -t -- ${words[1]} ;then fileT=-f ;else fileT=-d ;fi
+dircomp(){
+local -n cword=COMP_CWORD words=COMP_WORDS
+local now=${words[cword]} pre=${words[cword-1]}
+TYP=-d
+((cword>1)) &&type -t -- ${words[1]} &&TYP=-f
 IFS=$'\n'
 if [[ $now = ../* ]] ;then
- COMPREPLY=( $(compgen -W '$(compgen $fileT ../)' $now) )
+ COMPREPLY=( "$(compgen -W '$(compgen $TYP ../)' $now)" )
 elif [[ $now =~ ^([^1-9]*/)?([1-9][0-9]?)(.*) ]] ;then
  f=${BASH_REMATCH[1]}
  b=${BASH_REMATCH[3]}
- u=$f${DIRSTACK[BASH_REMATCH[2]]}$b
- COMPREPLY=( $(compgen -W "$(compgen $fileT $u)" $u) )
+ n=${BASH_REMATCH[2]}
+ u=${DIRSTACK[n]}
+ if [[ -d $n ]] &&[[ $b = @(/|//?*) || -z $u ]] ;then
+   COMPREPLY=( "$(compgen -W '$(compgen $TYP $n/)' $n${b#/})" )
+ elif [[ $u ]] ;then
+   u=$f$u
+   COMPREPLY=( "$(compgen -W '$(compgen $TYP $u/)' $u$b)" )
+ fi
 else
- exec 3< <(IFS=$'\n' dirs -l -p)
- COMPREPLY=( $(compgen -W "$(read -u3;while read -u3 d; f=$?;read w || ((! f)) ;do echo ${d// /\\\\ }${d:+/};echo ${w// /\\\\ }${w:+/} ;done< <(compgen -d);compgen -c)" $now) )
-fi
-} &>/dev/null &&complete -o default -o nosort -F _d m
+  exec 3< <(dirs -l -p)
+  COMPREPLY=( $(compgen -W "$(read -u3;while read -u3 d; f=$?;read w || ((! f)) ;do echo ${d// /\\\\ }${d:+/};echo ${w// /\\\\ }${w:+/} ;done< <(compgen -d);compgen -c)" $now) )
+fi} &>/dev/null &&complete -o default -o nosort -F dircomp m
