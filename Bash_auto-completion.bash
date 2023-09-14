e405b32f005f602565c1db6f6c89da32f02772b8
abdulbadii <abdulbadiik@gmail.com>
2023-09-14 18:52:04 +0700

    Update Bash_auto-completion.bash

--- Bash_auto-completion.bash
+++ Bash_auto-completion.bash
@@ -1,24 +1,23 @@
 dircomp(){
 local -n CWidx=COMP_CWORD words=COMP_WORDS
-local now=${words[CWidx]} pre=${words[CWidx-1]} TYP=-d
-((CWidx>1)) &&type -t -- ${words[1]} &&TYP=-f
+local now=${words[CWidx]} pre=${words[CWidx-1]} T=-d
+((CWidx>1)) &&type -t -- ${words[1]} &&T=-f
 IFS=$'\n'
 if [[ $now = ../* ]] ;then
- COMPREPLY=( $(compgen -W '$(compgen $TYP ../)' $now) )
+ COMPREPLY=( $(compgen -W '$(compgen $T ../)' $now) )
 elif [[ $now =~ ^([^1-9]*/)?([1-9][0-9]?)(.*) ]] ;then
  f=${BASH_REMATCH[1]}
  b=${BASH_REMATCH[3]}
  n=${BASH_REMATCH[2]}
- u=${DIRSTACK[n]}
- if [[ -d $f$n ]] &&[[ $b = @(/|//[!/]*) || -z $u ]] ;then
-  b=${b#/}
-  COMPREPLY=( $(compgen -d $f$n/${b#/}) )
- elif [[ $u ]] ;then
-   u=$f$u
-   COMPREPLY=( $(compgen -d $u$b/) )
+ if [[ $f || $b = @(/|//[!/]*) ]] || ((n>=${#DIRSTACK[@]})) ;then
+  [[ -d $f$n ]] &&{
+   b=${b#/}
+   COMPREPLY=( $(compgen -d $f$n/${b#/}) );}
+ else compopt -o nosort
+  COMPREPLY=( "$f${DIRSTACK[n]// /\\ }$b" $(compgen -d $f${DIRSTACK[n]}$b/) )
  fi
 elif [[ $now ]]  ;then
-  COMPREPLY=( $(compgen -o dirnames $TYP -- $now) )
+  COMPREPLY=( $(compgen -o dirnames $T -- $now) )
 else
   exec 3< <(dirs -l -p)
   compopt -o nosort
