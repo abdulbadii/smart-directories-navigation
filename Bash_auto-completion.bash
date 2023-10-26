1b504d996709098eba4234b1cc4724a603b2ce72
abdulbadii <abdulbadiik@gmail.com>
2023-10-26 20:25:35 +0700

    Update Bash_auto-completion.bash

--- Bash_auto-completion.bash
+++ Bash_auto-completion.bash
@@ -1,30 +1,35 @@
 dircomp(){
 local -n CWidx=COMP_CWORD words=COMP_WORDS
-local now=${words[CWidx]} pre=${words[CWidx-1]} T=-d
-((CWidx>1)) &&type -t -- ${words[1]} &&T=-f
-IFS=$'\n'
-if [[ $now = ../* ]] ;then
- COMPREPLY=( $(compgen -W '$(compgen $T ../)' $now) )
-elif [[ $now =~ ^([^1-9]*/)?([1-9][0-9]?)(.*) ]] ;then
- f=${BASH_REMATCH[1]}
- b=${BASH_REMATCH[3]}
- n=${BASH_REMATCH[2]}
- if [[ $f || $b = @(/|//[!/]*) ]] || ((n>=${#DIRSTACK[@]})) ;then
-  [[ -d $f$n ]] &&{
-   b=${b#/}
-   COMPREPLY=( $(compgen -d $f$n/${b#/}) );}
- else compopt -o nospace
-  COMPREPLY=( "$f${DIRSTACK[n]// /\\ }$b" $(compgen -d $f${DIRSTACK[n]}$b/) )
- fi
-elif [[ $now ]]  ;then
-  COMPREPLY=( $(compgen -o dirnames $T -- $now) )
-else
-  exec 3< <(dirs -l -p)
-  compopt -o nosort
-  COMPREPLY=( $(read -u3;while read -u3 d; f=$?;read w || ((! f)) ;do echo ${d// /\\ }${d:+/};echo ${w// /\\ }${w:+/} ;done< <(compgen -d)
-  if((CWidx==1)) ;then compgen -c; else for n in *;{ [[ -f $n ]] &&printf %s\\n "$n" ;};fi ) )
-  exec 3>&-
-fi
+local F G now=$2 T=-d IFS=$'\n'
+((CWidx>1)) &&[[ $(type -t -- ${words[1]}) ]] &&T=-f
+case $now in
+../*) compopt -o plusdirs;COMPREPLY=( $(compgen -X "../${PWD##*/}" $T $now) );;
+?([!1-9]*/)[1-9]?([0-9])*)
+  compopt -o plusdirs
+  [[ $now =~ ^([^1-9]*/)?([1-9][0-9]?)(.*) ]]
+  f=${BASH_REMATCH[1]}
+  n=${BASH_REMATCH[2]}
+  b=${BASH_REMATCH[3]}
+  if [[ $f || $b = @(/|//[!/]*) ]] || ((n>=${#DIRSTACK[@]})) ;then
+   [[ -d $f$n ]] &&{ b=${b#/}; COMPREPLY=( $(compgen -d $f$n/${b#/}) );}
+  else
+   COMPREPLY=(  $(compgen -d $f${DIRSTACK[n]}$b) )
+  fi;;
+*) COMPREPLY=( $(exec 3< <(dirs -l -p);read -u3
+ while :;do
+  ((!F)) &&{
+   if read w
+   then echo ${w// /\\ }/
+   else ((G))&&break;F=1;fi
+ }
+  ((!G)) &&{
+   if read -u3 d
+   then [[ $d = $now* ]] &&echo ${d// /\\ }/
+   else ((F))&&break;G=1;exec 3>&-;fi
+  }
+ done< <(compgen -d -- $now)
+ ((CWidx==1)) &&compgen -c -- $now) )
+esac
 } &>/dev/null &&complete -o default -o nosort -F dircomp g
 
 
